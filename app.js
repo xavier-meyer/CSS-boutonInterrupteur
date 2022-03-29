ee882c8702e8af9deab2f04f353dd67ed392762e
xavier-meyer <xavier.01@orange.fr>
2022-03-29 13:54:07 +0200

    init

---
+++ app.js
@@ -0,0 +1,7 @@
+
+let button = document.getElementById("buttonClick");
+let carre = document.getElementById("carre");
+
+button.addEventListener("click", function(){
+    carre.classList.toggle("mystyle");
+})
